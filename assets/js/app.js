3a5efd76384c5624c45e080b119364e9ff5acfa0
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-10-11 23:24:41 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,18 @@
 
 
 'use strict';
+
+
+// *@param { NodeList } $elements NodeList
+
+const addEventListener = function ($elements, eventType, collback) {
+    for (const $item of $elements) {
+        $item.addEventListener(eventType, collback);
+    }
+}
+
+const $header = document.querySelector("data-header");
+
+window.addEventListener("scroll", function () {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+})
