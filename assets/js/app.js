02e868970230ff66638b33778d43c89004bc847d
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-10-11 23:55:20 +0200

    skeleton

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,3 +24,17 @@ window.addEventListener("scroll", function () {
     $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 })
 
+
+// search toggle 
+
+const $searchToggler = document.querySelector("[data-search-toggler")
+const $searchField = document.querySelector("[data-search-field")
+
+let isExpanded = false;
+
+$searchToggler.addEventListener("click", function () {
+    $header.classList.toggle("search-active")
+    isExpanded = isExpanded ? false : true;
+    this.setAttribute("aria-expanded", isExpanded);
+    $searchField.focus()
+})
