3d887ed2cadf5f342ed7676c0b329fc6002ce9dd
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-10-13 16:15:13 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,3 +38,42 @@ $searchToggler.addEventListener("click", function () {
     this.setAttribute("aria-expanded", isExpanded);
     $searchField.focus()
 })
+
+
+// Tab navigation
+
+const $tabBtns = document.querySelectorAll("[data-tab-btn]")
+const $tabPanels = document.querySelectorAll("[data-tab-panel]")
+
+let [$lastActiveTabBtn] = $tabBtns
+let [$lastActiveTabPanel] = $tabPanels
+
+addEventOnElements($tabBtns, "click", function () {
+    $lastActiveTabBtn.setAttribute("aria-selected", "false");
+    $lastActiveTabPanel.setAttribute("hidden", "");
+
+    this.setAttribute("aria-selected", "true");
+    const $currentTabPanel = document.querySelector(`#${this.getAttribute("aria-controls")}`);
+    $currentTabPanel.removeAttribute("hidden")
+
+    $lastActiveTabBtn = this
+    $lastActiveTabPanel = $currentTabPanel
+})
+
+
+// keyboard accessibility for tab buttons
+
+addEventOnElements($tabBtns, "keydown", function (e) {
+    const $nextElement = this.$nextElementSibling;
+    const $previousElement = this.$previousElementSibling
+
+    if (e.key === "ArrowRight" && $nextElement) {
+        this.setAttribute("tabindex", "-1")
+        $nextElement.setAttribute("tabindex", "0");
+        $nextElement.focus();
+    } else if (e.key === "ArrowLeft" && $previousElement) {
+        this.setAttribute("tabindex", "-1")
+        $previousElement.setAttribute("tabindex", "0");
+        $previousElement.focus();
+    }
+})
