bb1f4fe1235ea2d3103b5c92a384daf7446f57fe
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-10-13 18:55:53 +0200

    s

--- assets/js/api.js
+++ assets/js/api.js
@@ -7,17 +7,6 @@
 
 'use strict';
 
-export async function fetchData(url, successCallback, errorCallback) {
-    const response = await fetch(url)
-
-    if (response.ok) {
-        const data = await response.json();
-        successCallback(data)
-    } else {
-        const error = await response.json()
-        errorCallback && errorCallback(error)
-    }
-}
 
 
 
